cc6287e6533c3bdb6a48e524cc750ad171469e20
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2020-12-12 20:53:49 +0200

    add workers table

---
+++ database/migrations/2020_12_12_143354_create_workers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->unsignedSmallInteger('age');
+            $table->string('address');
+            $table->string('phone')->unique();
+            $table->unsignedSmallInteger('salary');
+            $table->string('per');
+            $table->unsignedSmallInteger('store_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers');
+    }
+}
