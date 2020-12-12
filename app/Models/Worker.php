2e2c8a7ad648e68bb7e921a436b58ebec4fb56aa
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2020-12-12 20:53:35 +0200

    add Worker model

---
+++ app/Models/Worker.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Worker extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'age',
+        'address',
+        'phone',
+        'salary',
+        'per',
+        'store_id',
+    ];
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+}
