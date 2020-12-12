45fddb77d2133fa16f8e64e9cc5c30d412a154e6
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2020-12-12 20:59:56 +0200

    add worker views

---
+++ resources/views/admin/worker/create.blade.php
@@ -0,0 +1,173 @@
+@extends('layouts.admin')
+<title>@lang('translate.create_worker') | eCommerce</title>
+
+@section('content')
+    <div class="app-content content">
+        <div class="content-wrapper">
+            <div class="content-header row">
+                <div class="content-header-left col-md-6 col-12 mb-2">
+                    <div class="row breadcrumbs-top">
+                        <div class="breadcrumb-wrapper col-12">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a
+                                        href="{{ route('dashboard') }}">@lang('translate.dashboard')</a>
+                                </li>
+                                <li class="breadcrumb-item active">@lang('translate.create_worker')
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-body">
+                <section id="basic-form-layouts">
+                    <div class="row match-height">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title" id="basic-layout-form">@lang('translate.create_worker')
+                                    </h4>
+                                    <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                                    <div class="heading-elements">
+                                        <ul class="list-inline mb-0">
+                                            <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                            <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                            <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                            <li><a data-action="close"><i class="ft-x"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                @include('admin.includes.alerts.success')
+                                @include('admin.includes.alerts.errors')
+                                <div class="card-content collapse show">
+                                    <div class="card-body">
+                                        <form action="{{ route('workers.store') }}" method="POST">
+                                            @csrf
+
+                                            <div class="form-body">
+                                                <h4 class="form-section"><i
+                                                        class="ft-home"></i>@lang('translate.worker_data')</h4>
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>@lang('translate.worker_name')</label>
+                                                            <input type="text" value="{{ old('name') }}"
+                                                                class="form-control"
+                                                                placeholder="@lang('translate.worker_name_placeholder')"
+                                                                name="name">
+                                                            @error('name')
+                                                                <span class="text-danger">@lang('translate.'.$message)</span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>@lang('translate.worker_age')</label>
+                                                            <input type="text" value="{{ old('age') }}" class="form-control"
+                                                                placeholder="@lang('translate.worker_age_placeholder')"
+                                                                name="age" maxlength="2">
+                                                            @error('age')
+                                                                <span class="text-danger">@lang('translate.'.$message)</span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>@lang('translate.worker_address')</label>
+                                                            <input type="text" value="{{ old('address') }}"
+                                                                class="form-control"
+                                                                placeholder="@lang('translate.worker_address_placeholder')"
+                                                                name="address">
+                                                            @error('address')
+                                                                <span class="text-danger">@lang('translate.'.$message)</span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>@lang('translate.worker_phone')</label>
+                                                            <input type="text" value="{{ old('phone') }}"
+                                                                class="form-control"
+                                                                placeholder="@lang('translate.worker_phone_placeholder')"
+                                                                name="phone" maxlength="11">
+                                                            @error('phone')
+                                                                <span class="text-danger">@lang('translate.'.$message)</span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>@lang('translate.worker_salary')</label>
+                                                            <input type="text" value="{{ old('salary') }}"
+                                                                class="form-control"
+                                                                placeholder="@lang('translate.worker_salary_placeholder')"
+                                                                name="salary" maxlength="5">
+                                                            @error('salary')
+                                                                <span class="text-danger">@lang('translate.'.$message)</span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>@lang('translate.worker_per')</label>
+                                                            <select class="form-control" name="per">
+                                                                <option value="week">Week</option>
+                                                                <option value="month">Month</option>
+                                                                <option value="year">Year</option>
+                                                            </select>
+                                                            @error('per')
+                                                                <span class="text-danger">@lang('translate.'.$message)</span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <div class="form-group">
+                                                            <label>@lang('translate.store_name')</label>
+                                                            <select class="form-control" name="store_id">
+                                                                <option value="0">none</option>
+                                                                @foreach ($stores as $store)
+                                                                    <option value="{{ $store->id }}">
+                                                                        {{ $store->name }}
+                                                                    </option>
+                                                                @endforeach
+                                                            </select>
+                                                            @error('store_id')
+                                                                <span class="text-danger">@lang('translate.'.$message)</span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-actions">
+                                                    <button type="button" class="btn btn-warning mr-1"
+                                                        onclick="history.back();">
+                                                        <i class="ft-x"></i> @lang('translate.cancel')
+                                                    </button>
+                                                    <button type="submit" class="btn btn-primary">
+                                                        <i class="la la-check-square-o"></i> @lang('translate.save')
+                                                    </button>
+                                                </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/admin/worker/index.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.admin')
+<title>@lang('translate.workers') | eCommerce</title>
+
+@push('script')
+    <script>
+        $(document).ready(function() {
+            $('#tblData').DataTable({
+                "scrollX": true
+            });
+        });
+
+    </script>
+@endpush
+
+@section('content')
+    <div class="app-content content">
+        <div class="content-wrapper">
+            <div class="content-header row">
+                <div class="content-header-left col-md-6 col-12 mb-2">
+                    <div class="row breadcrumbs-top">
+                        <div class="breadcrumb-wrapper col-12">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a
+                                        href="{{ route('dashboard') }}">@lang('translate.dashboard')</a>
+                                </li>
+                                <li class="breadcrumb-item active">@lang('translate.workers')</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-body">
+                <section id="dom">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">@lang('translate.workers')</h4>
+                                    <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                                    <div class="heading-elements">
+                                        <ul class="list-inline mb-0">
+                                            <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                            <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                            <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                            <li><a data-action="close"><i class="ft-x"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                @include('admin.includes.alerts.success')
+                                @include('admin.includes.alerts.errors')
+                                <div class="card-content collapse show">
+                                    <div class="card-body card-dashboard">
+                                        <table id="tblData">
+                                            <thead>
+                                                <tr>
+                                                    <th>@lang('translate.worker_name')</th>
+                                                    <th>@lang('translate.worker_age')</th>
+                                                    <th>@lang('translate.worker_phone')</th>
+                                                    <th>@lang('translate.worker_address')</th>
+                                                    <th>@lang('translate.worker_salary')</th>
+                                                    <th>@lang('translate.worker_per')</th>
+                                                    <th>@lang('translate.store_name')</th>
+                                                    <th>@lang('translate.measures')</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($workers as $worker)
+                                                    <tr>
+                                                        <td>{{ $worker->name }}</td>
+                                                        <td>{{ $worker->age }}</td>
+                                                        <td>{{ $worker->phone }}</td>
+                                                        <td>{{ $worker->address }}</td>
+                                                        <td>{{ $worker->salary }}</td>
+                                                        <td>{{ $worker->per }}</td>
+                                                        <td>
+                                                            @if ($worker->store_id == 0)
+                                                                none
+                                                            @else
+                                                                {{ $worker->store->name }}
+                                                            @endif
+                                                        </td>
+                                                        <td>
+                                                            @include('admin.includes.formMeasures', ['editRouting' =>
+                                                            'workers.edit', 'destroyRouting' => 'workers.destroy',
+                                                            'idValue' => $worker->id])
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+@endsection
