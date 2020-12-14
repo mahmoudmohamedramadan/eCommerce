ebac1c256c532d66c461f24e58132e4cb5406bce
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2020-12-14 16:51:25 +0200

    add debts table

---
+++ database/migrations/2020_12_14_141410_create_debts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDebtsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('debts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->text('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('debts');
+    }
+}
