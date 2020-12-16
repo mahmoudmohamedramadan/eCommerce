9f10ee290247f3310ba5b82c07d1ba4b6ef3cc3c
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2020-12-16 20:14:24 +0200

    update Main route

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,9 +1,9 @@
-<div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true">
+<div class="main-menu menu-fixed menu-light menu-accordion menu-shadow " data-scroll-to-active="true">
     <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
 
-            <li class="nav-item active"><a href=""><i class="la la-mouse-pointer"></i><span class="menu-title"
-                        data-i18n="nav.add_on_drag_drop.main">@lang('translate.main')</span></a>
+            <li class="nav-item active"><a href="{{ route('dashboard') }}"><i class="la la-mouse-pointer"></i><span
+                        class="menu-title" data-i18n="nav.add_on_drag_drop.main">@lang('translate.main')</span></a>
             </li>
 
             <li class="nav-item"><a href=""><i class="la la-cubes"></i>
