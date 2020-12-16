5a72c59e96559d048094ed6f9b98c6e4e9b72676
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2020-12-16 13:29:51 +0200

    remove notifications list

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -3,9 +3,11 @@
     <div class="navbar-wrapper">
         <div class="navbar-header">
             <ul class="nav navbar-nav flex-row">
-                <li class="nav-item mobile-menu d-md-none mr-auto"><a
-                        class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i
-                            class="ft-menu font-large-1"></i></a></li>
+                <li class="nav-item mobile-menu d-md-none mr-auto">
+                    <a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i
+                            class="ft-menu font-large-1"></i></a>
+                </li>
+
                 <li class="nav-item">
                     <a class="navbar-brand" href="index.html">
                         <img class="brand-logo" alt="modern admin logo"
@@ -13,18 +15,21 @@
                         <h3 class="brand-text">@lang('translate.admin_panel')</h3>
                     </a>
                 </li>
+
                 <li class="nav-item d-md-none">
                     <a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i
                             class="la la-ellipsis-v"></i></a>
                 </li>
             </ul>
         </div>
+
         <div class="navbar-container content">
             <div class="collapse navbar-collapse" id="navbar-mobile">
                 <ul class="nav navbar-nav mr-auto float-left">
                     <li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs"
                             href="#"><i class="ft-menu"></i></a></li>
                 </ul>
+
                 <ul class="nav navbar-nav">
                     <li class="dropdown dropdown-user nav-item">
                         <a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
@@ -63,7 +68,7 @@
                     <li class="dropdown dropdown-notification nav-item mr-5">
                         <a class="nav-link nav-link-label" href="#" data-toggle="dropdown"><i class="ficon ft-bell"></i>
                             <span class="badge badge-pill badge-default badge-danger badge-default badge-up badge-glow">
-                                0
+                                {{ auth()->user()->unreadNotifications->count() }}
                             </span>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
@@ -71,16 +76,12 @@
                                 <h6 class="dropdown-header m-0">
                                     <span class="grey darken-2">@lang('translate.notifications')</span>
                                 </h6>
-                                <span class="notification-tag badge badge-default badge-danger float-right m-0">0
-                                    New</span>
+                                <span class="notification-tag badge badge-default badge-danger float-right m-0">
+                                    {{ auth()->user()->unreadNotifications->count() }} New</span>
                             </li>
-                            <li class="scrollable-container media-list w-100">
-                                <a href="javascript:void(0)">
-                                    <div class="media">
 
-                                    </div>
-                                </a>
-                            </li>
+                            <li class="scrollable-container media-list w-100"></li>
+
                             <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center"
                                     href="javascript:void(0)">@lang('translate.read_notifications')</a>
                             </li>
