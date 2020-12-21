c6e0a9a7188f49f05f7b7afafba24959bc46d326
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2020-12-21 17:00:37 +0200

    remove pushNotifications

--- app/Http/Controllers/Admin/DebtController.php
+++ app/Http/Controllers/Admin/DebtController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\DebtRequest;
-use App\Notifications\DebtNotification;
 use Illuminate\Support\Facades\DB;
 use App\Models\Admin;
 use App\Models\Debt;
@@ -133,67 +132,5 @@ class DebtController extends Controller
             session()->flash('error', __('translate.deleted_error'));
             return redirect()->route('debts.index');
         }
-    }
-
-    /**
-     *
-     * Push notification into database
-     * @return void
-     */
-    public function pushNotifications()
-    {
-        $UnNotifiedDebts = Debt::unnotifiedDebts()->get();
-
-        if (isset($UnNotifiedDebts)) {
-            foreach ($UnNotifiedDebts as $debt) {
-                Admin::first()->notify(new DebtNotification($debt));
-
-                $debt->update([
-                    'notified' => true
-                ]);
-            }
-        }
-
-        $notifications = Admin::first()->unreadNotifications;
-
-        if (isset($notifications)) {
-            $noificationView = view('admin.includes.notifications', ['notifications' => $notifications])
-                ->render();
-
-            return response()->json([
-                'success' => true,
-                'notifications_count' => count($notifications),
-                'notifications' => $noificationView
-            ]);
-        }
-
-        return response()->json([
-            'success' => true,
-            'notifications_count' => 0,
-            'notifications' => null
-        ]);
-    }
-
-    /**
-     *
-     * Read all debt notifications
-     */
-    public function readAllNotifications()
-    {
-        if (Admin::first()->unreadNotifications->count() > 0) {
-            Admin::first()->unreadNotifications->each(function ($notification) {
-                $notification->markAsRead();
-            });
-
-            return response()->json([
-                'success' => true,
-                'notifications_count' => 0,
-                'notifications' => null
-            ]);
-        }
-
-        return response()->json([
-            'success' => true
-        ]);
     }
 }
