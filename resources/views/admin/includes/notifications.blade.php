b8cdf8ce581d93d1e6c1459fe68b8de2144fe3c6
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2020-12-21 17:06:16 +0200

    add media-body in notifications view for product minimum

--- resources/views/admin/includes/notifications.blade.php
+++ resources/views/admin/includes/notifications.blade.php
@@ -4,13 +4,24 @@
             <i class="ft-plus-square icon-bg-circle bg-cyan"></i>
         </div>
 
-        <div class="media-body">
-            <a
-                href="{{ route('debts.show', ['debt' => $notification['data']['id'], 'notification_id' => $notification->id]) }}">
-                <h6 class="media-heading">
-                    @lang('translate.debt_title'): {{ $notification['data']['title'] }}
-                </h6>
-            </a>
-        </div>
+        @if (array_key_exists('debt_id', $notification['data']))
+            <div class="media-body">
+                <a
+                    href="{{ route('debts.show', ['debt' => $notification['data']['debt_id'], 'notification_id' => $notification->id]) }}">
+                    <h6 class="media-heading">
+                        @lang('translate.debt_title'): {{ $notification['data']['debt_title'] }}
+                    </h6>
+                </a>
+            </div>
+        @else
+            <div class="media-body">
+                <a
+                    href="{{ route('products.show', ['product' => $notification['data']['product_id'], 'notification_id' => $notification->id]) }}">
+                    <h6 class="media-heading">
+                        @lang('translate.has_reached_its_minimum') {{ $notification['data']['product_title'] }}
+                    </h6>
+                </a>
+            </div>
+        @endif
     </div>
 @endforeach
