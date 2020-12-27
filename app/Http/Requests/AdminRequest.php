96baafd2c32d4e966d5f2c86dc0d47ef9fd22d2e
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2020-12-27 12:52:14 +0200

    update AdminRequest

--- app/Http/Requests/AdminRequest.php
+++ app/Http/Requests/AdminRequest.php
@@ -37,11 +37,11 @@ class AdminRequest extends FormRequest
     public function messages()
     {
         return [
-            'name.required' => 'name field is required',
-            'name.max' => 'name may not be greater than 25 characters',
+            'name.required' => 'name_field_is_required',
+            'name.max' => 'name_may_not_be_greater_than_25_characters',
 
-            'email.required' => 'email field is required',
-            'email.email' => 'email field should be correct email',
+            'email.required' => 'email_field_is_required',
+            'email.email' => 'email_field_should_be_correct_email',
         ];
     }
 }
