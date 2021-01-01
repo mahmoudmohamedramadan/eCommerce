cf5b8cc4e0f8bace882c87127aaf41bccb7ae27f
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-01-01 14:15:11 +0200

    add WorkersOfStore controller

--- app/Http/Controllers/Admin/WorkersOfStoreController.php
+++ app/Http/Controllers/Admin/WorkersOfStoreController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Models\Worker;
 use App\Models\Store;
 
 class WorkersOfStoreController extends Controller
@@ -14,12 +16,61 @@ class WorkersOfStoreController extends Controller
      */
     public function index(Store $store)
     {
+        $stores = Store::get();
         $workers = array();
 
         foreach ($store->workers as $worker) {
             array_push($workers, $worker);
         }
 
-        return view('admin.worker.index', compact('workers'));
+        return view('admin.store_workers.index', compact('workers', 'stores'))->with('store_id', $store->id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Models\Store  $store
+     * @param  \App\Models\Worker  $worker
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Store $store, Worker $worker)
+    {
+        try {
+            DB::beginTransaction();
+            $worker->update([
+                'store_id' => request()->get('store_id')
+            ]);
+            DB::commit();
+
+            session()->flash('success', __('translate.updated_success'));
+            return redirect()->route('stores.workers.index', $store->id);
+        } catch (\Exception $ex) {
+            DB::rollBack();
+            session()->flash('success', __('translate.updated_error'));
+            return redirect()->route('stores.workers.index', $store->id);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Store  $store
+     * @param  \App\Models\Worker  $worker
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Store $store, Worker $worker)
+    {
+        try {
+            DB::beginTransaction();
+            $worker->delete();
+            DB::commit();
+
+            session()->flash('success', __('translate.deleted_success'));
+            return redirect()->route('stores.workers.index', $store->id);
+        } catch (\Exception $ex) {
+            DB::rollBack();
+            session()->flash('error', __('translate.deleted_error'));
+            return redirect()->route('stores.workers.index', $store->id);;
+        }
     }
 }
